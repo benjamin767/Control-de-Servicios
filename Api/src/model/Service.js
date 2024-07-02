294f6a92f6e6fcde397a7ff423a425176f51a15e
Benjamin <natanael.96.01@gmail.com>
2024-07-02 11:37:47 -0300

    modified Service model

--- Api/src/model/Service.js
+++ Api/src/model/Service.js
@@ -2,7 +2,47 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
     sequelize.define('service', {
-
+        rubro: {
+            type: DataTypes.STRING,
+        },
+        empresa: {
+            type: DataTypes.STRING,
+        },
+        descripcion,
+        periodo: {
+            type: DataTypes.STRING,
+            defaultValue: 'Mensual',
+            validate: {
+                isIn: [['Mensual', 'Anual']],
+            }
+        },
+        metodo_de_pago: {
+            type: DataTypes.STRING,
+            defaultValue: 'Manual',
+            validate: {
+                isIn: [['Manual', 'Automatico']],
+            }
+        },
+        medio_de_pago: {
+            type: DataTypes.STRING,
+            defaultValue: 'Transferencia',
+            validate: {
+                isIn: [['Debito Automatico en Cuenta', 'Debito Automatico en Tarjeta', 'Transferencia', 'Tarjeta de Credito']],
+            }
+        },
+        vencimiento: {
+            type: DataTypes.DATEONLY,
+        },
+        moneda: {
+            type: DataTypes.STRING,
+            defaultValue: 'Pesos',
+            validate: {
+                isIn: [['Pesos', 'Dolares', 'Euros']],
+            }
+        },
+        importe: {
+            type: DataTypes.STRING,
+        },
     }, {
         timestamps: false,
     });
