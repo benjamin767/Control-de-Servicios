8e0cead30eb59043452442262c61b2280103eadb
Benjamin <natanael.96.01@gmail.com>
2024-07-02 10:54:46 -0300

    first commit

---
+++ Api/src/routes/index.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+// const trademarkMiddleware = require('./Trademark');
+// Ejemplo: const auth Router = require('./auth.js');
+// Ejemplo: const authRouter = require('./auth.js');
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+                                                                                                                                                                                                                                                                    
+// router.use('/trademarks', trademarkMiddleware);
+
+
+module.exports = router;
