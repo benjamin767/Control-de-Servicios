7cf0e9838fa71d7c64448395c714b79444fe9e1d
Benjamin <natanael.96.01@gmail.com>
2024-07-15 11:39:22 -0300

    added sript js & form to create Services

--- Client/src/views/newTicket.js
+++ Client/src/views/newTicket.js
@@ -1,6 +1,14 @@
- const button = document.querySelector(".btn");
- button.addEventListener("click", e => {
-    e.preventDefault();
-    console.log("se hizo el envio de datos");
- });
+const button = document.querySelector(".btn");
+button.addEventListener("click", e => {
+   e.preventDefault();
+   const rubro = document.querySelector("#rubro").value;
+   const empresa = document.querySelector("#empresa").value;
+   const descripcion = document.querySelector("#descripcion").value;
+   const metodo_de_pago = document.querySelector("#metedoDePago").value;
+   const medio_de_pago = document.querySelector("#medioDePago").value;
+   const moneda = document.querySelector("#moneda").value;
+   const importe = document.querySelector("#importe").value;
+   const estado = document.querySelector("#estado").value;
+   console.log(rubro, empresa, descripcion, medio_de_pago, metodo_de_pago, moneda, importe, estado);
+});
  
