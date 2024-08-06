ee35b0b0276fa0b580cf0d4804b50b6c47569c61
Benjamin <natanael.96.01@gmail.com>
2024-08-06 13:41:31 -0300

    Added modal to newRubro & newEmpresa

--- Client/src/views/newRubro.js
+++ Client/src/views/newRubro.js
@@ -1,5 +1,6 @@
 const button = document.querySelector(".btn");
 const nombre = document.querySelector("#nombre");
+const modalText = document.querySelector(".modal-body");
 
 button.addEventListener("click", e => {
     e.preventDefault();
@@ -11,5 +12,21 @@ button.addEventListener("click", e => {
             body: JSON.stringify({
                 nombre: nombre.value
             })
-    })
+    }).then(res => {
+        if (res.status === 400 || res.status === 404) modalText.textContent = "Ups, algo salio mal :|";
+        else modalText.textContent = "Rubro creado y guardado! :)";
+    }).catch(error => {
+        modalText.textContent = "Ups, algo salio mal :|";
+        console.log(error);
+    });
 })
+
+document.getElementById("advertenciaModal").addEventListener("hidden.bs.modal", function() {
+    // Vaciar los campos del formulario
+    const formulario = document.querySelector("form");
+    const campos = formulario.querySelectorAll("input, select");
+    campos.forEach(function(campo) {
+       campo.value = '';
+    });
+    modalText.textContent = "Cargando solicitud...";
+});
